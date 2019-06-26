83a25706fee5e51c0e35c25fcb02406dedceea7b
Daniel vianney <47211214+daniel10027@users.noreply.github.com>
2019-06-26 14:43:23 +0000

    Data-base-connect
    Connexion à une base de donnés MySQL en local

---
+++ database.php
@@ -0,0 +1,37 @@
+<?php
+	
+	/*
+	  connexion a la database
+	*/
+		class Database
+	{ 	
+		private static	$dbHost = "localhost";
+		private static	$dbName = "le_nom_de_la_base";
+		private static	$dbUser = "root";
+		private static	$dbUserPassword = "";
+		private static	$connection = null ;
+				public static function connect()
+				{
+					try 
+					{
+						self::$connection = new PDO("mysql:host=".self::$dbHost .";dbname=". self::$dbName,self::$dbUser ,self::$dbUserPassword);
+					} 
+					catch (PDOException $e) 
+					{
+						die($e->getMessage());
+					}
+					return self::$connection ;
+				}
+				function disconnect()
+				{
+					self::$connection = null ;
+				}
+		
+
+
+
+
+
+
+
+
